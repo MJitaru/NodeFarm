b56cc8db7ea3775de9b327a08ef347432e251f7b
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-06-23 13:08:41 +0300

    2nd commit

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ console.log('Will read file!');*/
 
 ///////////////////////////////////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName );
+    output = output.replace (/{%IMAGE%}/g, product.image);
+    output = output.replace (/{%PRICE%}/g, product.price);
+    output = output.replace (/{%FROM%}/g, product.from);
+    output = output.replace (/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace (/{%QUANTITY%}/g, product.quantity);
+    output = output.replace (/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace (/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
 //Very simple API
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -42,20 +54,25 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req,res)=> {
-    const pathName = req.url;
+    const {query, pathname} = url.parse(req.url, true);
 
 //Overview page
     
-    if (pathName === '/' || pathName === '/overview'){
-        res.writeHead(200, {'Content-type' : 'txt/html'});
-        res.end (tempOverview);
+    if (pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {'Content-type' : 'text/html'});
+
+       const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+       const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+       
+
+        res.end (output);
 
 //Product page'
-    } else if (pathName === '/product'){
+    } else if (pathname === '/product'){
         res.end(tempProduct);
 
 //API
-    } else if (pathName ==='/api') {
+    } else if (pathname ==='/api') {
         res.writeHead(200, {'Content-type' : 'application/json'});
         res.end(data);
         
